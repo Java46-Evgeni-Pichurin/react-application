f3aa99eb66d1acf20d821d2b350cd7db66f1aec0
Evgeni Pichurin <pichurinaa@gmail.com>
2022-05-17 18:01:48 +0300

    HW-43

--- src/components/forms/CourseForm.tsx
+++ src/components/forms/CourseForm.tsx
@@ -1,25 +1,28 @@
 import React from "react";
 import { Course, createCourse } from "../../models/Course";
 import courseData from "../../config/courseData.json";
-import { Grid, Select, TextField, FormControl, InputLabel, MenuItem, Button, FormLabel, Typography } from "@mui/material";
-import { Box, display, margin, padding, textAlign } from "@mui/system";
+import { Grid, Select, TextField, FormControl, InputLabel, MenuItem, Button, Box, Typography } from "@mui/material";
+import { getRandomNumber } from "../../util/random";
 type Props = {
-    submitFn: (course: Course) => void,
-    initCourse?: Course
+    submitFn: (course: Course) => void;
+    courseUpdate?: Course
 }
-const initialCourse: Course = createCourse(0, "", "", 0, 0, new Date());
-const CourseForm: React.FC<Props> = ({ submitFn, initCourse }) => {
+const initialCourse: Course = createCourse(0, "",
+    "", 0, 0, new Date("0000-00-00"));
+const CourseForm: React.FC<Props> = ({ submitFn, courseUpdate }) => {
     const { courses, minHours, maxHours, lectors, minCost, maxCost, minYear, maxYear } = courseData;
-    const [course, setCourse] = React.useState(initCourse || initialCourse);
-    const formTitle = initCourse ? `Updating course with id ${course.id}` : 'Adding New Course';
+    const [course, setCourse] = React.useState(courseUpdate || initialCourse);
+
     function onSubmit(event: any) {
         event.preventDefault();
+        console.log(course)
         submitFn(course);
         document.querySelector('form')!.reset();
     }
     function handlerCourse(event: any) {
         const courseCopy = { ...course };
         courseCopy.name = event.target.value;
+        console.log(courseCopy.name)
         setCourse(courseCopy);
     }
     function handlerLecturer(event: any) {
@@ -43,24 +46,28 @@ const CourseForm: React.FC<Props> = ({ submitFn, initCourse }) => {
         setCourse(courseCopy);
     }
     function onReset() {
-        setCourse(initCourse || initialCourse)
+        setCourse(courseUpdate || initialCourse)
+        
     }
 
-    return <form onSubmit={onSubmit} onReset={onReset}>
-        <InputLabel id="form-label">
-            <Typography variant="h4" align="center" sx={{marginBottom: '1vh'}}>{formTitle}</Typography>
-        </InputLabel>
-        <Grid container spacing={{ xs: 5, sm: 2, md: 13 }} justifyContent="center">
+    return <Box>
+        <Typography gutterBottom variant={'h4'}
+         sx={{fontSize: {xs: "1.3em", sm: "1em", md:"2em"}, textAlign: 'center', fontWeight: 'bold'}}>
+             {!!courseUpdate ? `Update of Course with id ${courseUpdate.id}` : 'Adding new Course'}</Typography>
+        <form onSubmit={onSubmit} onReset={onReset}>
+        <Grid container spacing={{ xs: 5, sm: 1.5, md: 13 }} justifyContent="center">
             <Grid item xs={10} sm={5} >
-                <FormControl fullWidth required disabled={course === initCourse}>
+                <FormControl fullWidth required>
                     <InputLabel id="course-select-label">Course Name</InputLabel>
                     <Select
-                        disabled={course === initCourse}
                         labelId="course-select-label"
                         id="demo-simple-select"
                         label="Course Name"
                         value={course.name}
                         onChange={handlerCourse}
+                        inputProps={
+                            { readOnly: !!courseUpdate }
+                        }
                     >
                         <MenuItem value="">None</MenuItem>
                         {getCourseItems(courses)}
@@ -85,17 +92,19 @@ const CourseForm: React.FC<Props> = ({ submitFn, initCourse }) => {
             <Grid item xs={10} sm={5}>
                 <TextField type="number" label="Hours" fullWidth required
                     onChange={handlerHours} helperText={`enter number of hours in range [${minHours}-${maxHours} ]`}
-                    value={course.hours || undefined}
+                    value={course.hours || ''}
                     inputProps={{
+
                         min: `${minHours}`,
                         max: `${maxHours}`
                     }} />
             </Grid>
             <Grid item xs={10} sm={5}>
                 <TextField type="number" label="Cost" fullWidth required
+                    value={course.cost || ""}
                     onChange={handlerCost} helperText={`enter cost in range [${minCost}-${maxCost} ]`}
-                    value={course.cost || undefined}
                     inputProps={{
+
                         min: `${minCost}`,
                         max: `${maxCost}`
                     }} />
@@ -103,16 +112,15 @@ const CourseForm: React.FC<Props> = ({ submitFn, initCourse }) => {
             <Grid item xs={10} sm={8} >
                 <TextField required label={'Opening Date'} type={'date'} fullWidth
                     onChange={handlerOpeningDate}
-                    value={undefined}
-                    inputProps={
-                        {
-                            min: `${minYear}-1-01`,
-                            max: `${maxYear}-12-31`
-                        }
-                    }
-                    InputLabelProps={{
-                        shrink: true
-                    }} />
+                    value={!!course.openingDate.getFullYear() ?
+                        getIsoDate(course.openingDate) : ''} inputProps={
+                            {
+                                min: `${minYear}-01-01`,
+                                max: `${maxYear}-12-31`
+                            }
+                        } InputLabelProps={{
+                            shrink: true
+                        }} />
 
             </Grid>
             <Grid item xs={10} sm={8} md={6}>
@@ -123,13 +131,28 @@ const CourseForm: React.FC<Props> = ({ submitFn, initCourse }) => {
                     <Grid item xs={5}>
                         <Button type="reset">Reset</Button>
                     </Grid>
+
                 </Grid>
+
             </Grid>
+
+
+
         </Grid>
     </form>
+        </Box>
 }
 export default CourseForm;
 
 function getCourseItems(items: string[]): React.ReactNode {
     return items.map(c => <MenuItem value={c} key={c}>{c}</MenuItem>)
 }
+function getIsoDate(dateValue: Date): string {
+    const day = dateValue.getDate() + 1;
+    const month = dateValue.getMonth();
+    const year = dateValue.getFullYear();
+    const dateUTC = new Date(year, month, day);
+    return dateUTC.toISOString().substring(0, 10);
+
+}
+
--- src/components/pages/Courses.tsx
+++ src/components/pages/Courses.tsx
@@ -1,53 +1,114 @@
 import React, { useCallback } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { coursesService } from "../../config/service-config";
 import { Course } from "../../models/Course";
 import { StateType } from "../../redux/store";
 import { DataGrid, GridColumns, GridRowParams, GridActionsCellItem } from '@mui/x-data-grid'
-import { Box, Paper } from "@mui/material";
-import { Delete, Edit } from "@mui/icons-material";
+import { Box, List, ListItem, Modal, Paper, useMediaQuery } from "@mui/material";
+import { Delete, Edit, Visibility } from "@mui/icons-material";
 import { removeCourse, updateCourse } from "../../redux/actions";
 import CourseForm from "../forms/CourseForm";
-
-function getActions(actionsFn: (params: GridRowParams) => JSX.Element[]): GridColumns {
+import ActionConfirmation from "../dialogs/ActionConfirmation";
+import ConfirmationData from "../../models/ConfirmationData";
+function getActions(actionsFn: (params: GridRowParams)=>JSX.Element[]): GridColumns {
     const columns: GridColumns = [
-        { field: "name", type: "string", headerName: "Course Name", align: "center", headerAlign: "center", flex: 1 },
-        { field: "lecturer", type: "string", headerName: "Lecturer", align: "center", headerAlign: "center", flex: 0.7 },
-        { field: "hours", type: "number", headerName: "Hours", align: "right", headerAlign: "center", flex: 0.5 },
-        { field: "cost", type: "number", headerName: "Cost", align: "right", headerAlign: "center", flex: 0.6 },
-        { field: "openingDate", type: "date", headerName: "Date", align: "center", headerAlign: "center", flex: 0.7 },
-        { field: "actions", type: "actions", flex: 0.5, getActions: actionsFn }
+    { field: "name", type: "string", headerName: "Course Name", align: "center", headerAlign: "center", flex:1 },
+    { field: "lecturer", type: "string", headerName: "Lecturer", align: "center", headerAlign: "center", flex: 0.7 },
+    { field: "hours", type: "number", headerName: "Hours", align: "right", headerAlign: "center", flex: 0.5 },
+    { field: "cost", type: "number", headerName: "Cost", align: "right", headerAlign: "center", flex: 0.6 },
+    { field: "openingDate", type: "date", headerName: "Date", align: "center", headerAlign: "center", flex: 0.7 },
+    { field: "actions", type: "actions", flex: 0.5, getActions:actionsFn}
 
-    ]
-    return columns
+]
+return columns
 }
-
+const style = {
+    position: 'absolute' as 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+  };
 const Courses: React.FC = () => {
     const dispatch = useDispatch()
     const courses: Course[] = useSelector<StateType, Course[]>(state => state.courses);
     const [isEdit, setEdit] = React.useState(false);
-    const [course, setCourse] = React.useState(courses[0])
+    const [flOpen, setFlOpen] = React.useState<boolean>(false);
+    const [modalOpen, setModalOpen] = React.useState<boolean>(false);
+    const confirmationData = React.useRef<ConfirmationData>({title:'', content:'',confirmHandler:()=>{}});
+    const updatedCourse = React.useRef<Course>();
+    const shownCourse = React.useRef<Course>();
     function actionsFn(params: GridRowParams): JSX.Element[] {
         const actionElements: JSX.Element[] = [
-            <GridActionsCellItem label="Remove" onClick={() => dispatch(removeCourse(params.id as number))} icon={<Delete />} />,
-            <GridActionsCellItem label="Edit" onClick={() => editFn(params.id as number)} icon={<Edit />} />
+            <GridActionsCellItem label="Remove" onClick={() => showRemoveConfirmation(params.id as number)}
+             icon={<Delete/>}/>,
+             <GridActionsCellItem label="Edit" onClick={() => editFn(params.id as number)} icon={<Edit/>}/>,
+             <GridActionsCellItem label="Details" icon={<Visibility/>}
+              onClick={showDetails.bind( undefined, params.id as number)}/>
         ]
         return actionElements;
     }
+    function showDetails(id: number) {
+        shownCourse.current = courses.find(c => c.id === id);
+        setModalOpen(true);
+    }
     function editFn(id: number) {
-        setCourse(courses.find(c => c.id === id) as Course)
+        updatedCourse.current = courses.find(c => c.id === id)
         setEdit(true);
     }
-    function onSubmit(course: Course) {
-        dispatch(updateCourse(course));
+    function showUpdateConfirmation(course: Course) {
+        confirmationData.current.confirmHandler = updateAction.bind(undefined, course);
+        confirmationData.current.title = 'Updating Course Confirmation'
+        confirmationData.current.content = `You are going to update course with id ${course.id}`;
+        setFlOpen(true);
         setEdit(false)
+    }
+    function updateAction(course: Course, flConfirm: boolean): void {
+        if(flConfirm) {
+            dispatch(updateCourse(course))
+        }
+        setFlOpen(false)
+    }
+    function showRemoveConfirmation(id: number) {
+        confirmationData.current.confirmHandler = removeAction.bind(undefined, id);
+        confirmationData.current.title = 'Remove Course Confirmation'
+        confirmationData.current.content = `You are going to remove course with id ${id}`;
+        setFlOpen(true);
+    }
+    function removeAction(id: number, flConfirm: boolean): void {
+        if (flConfirm) {
+            dispatch(removeCourse(id))
+        }
+        setFlOpen(false)
     }
     const getActionsCallback = useCallback(getActions, [courses]);
     const columns = getActionsCallback(actionsFn);
-    return <Box sx={{ display: 'flex', justifyContent: 'center' }}><Paper sx={{ height: { xs: '70vh', sm: '85vh', md: '80vh' }, width: { xs: '100%', md: '80%' } }}>
-        {isEdit ? <CourseForm submitFn={onSubmit} initCourse={course} /> : <DataGrid rows={courses} columns={columns} />}
-    </Paper></Box>
+    // const smallWidth = useMediaQuery('(min-width: 600px)');
+    // const mediumWidth = useMediaQuery('(min-width: 900px)');
+    // const largeWidth = useMediaQuery('(min-width: 900px)');
+    return <Box sx={{display: 'flex', justifyContent: 'center' }}><Paper sx={{height: {xs: '90vh', sm: '85vh', md: '80vh'}, width: {xs: '100%', md: '80%'}}}>
+        {isEdit ? <CourseForm submitFn={showUpdateConfirmation}
+             courseUpdate={updatedCourse.current}/> : <DataGrid rows={courses} columns={columns} />}
+    </Paper>
+    <ActionConfirmation open={flOpen} title={confirmationData.current.title}
+     content={confirmationData.current.content} confirmHandler={confirmationData.current.confirmHandler}/>
+     <Modal
+        open={modalOpen}
+        onClose={()=>setModalOpen(false)}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <List>
+              {shownCourse.current && Object.entries(shownCourse.current as any).map(e => <ListItem key={e[0]}>{`${e[0]}: ${e[1]}`}</ListItem>)}
+          </List>
+        </Box>
+      </Modal>
+    </Box>
 
 }
 export default Courses;
-
-
